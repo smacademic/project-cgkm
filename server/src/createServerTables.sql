d5f48b2add003d53826fb9114d7824fc48c3417a
jgrabowski121 <justingrabowski@yahoo.com>
2019-03-20 14:52:00 -0400

    Fix bugs in createServerTables.sql
    Fix typos, as well as adding header comments to each create table query. Changed User table to ArcUser and added UID to the Task table for correctness

--- server/src/createServerTables.sql
+++ server/src/createServerTables.sql
@@ -33,40 +33,44 @@
 START TRANSACTION;
 
 
-CREATE TABLE User
+CREATE TABLE ArcUser
 (
-   UID VARCHAR(60) PRIMARY KEY CHECK(LENGTH((TRIM(UID)) = 60)),
+   UID VARCHAR(60) PRIMARY KEY CHECK(CHAR_LENGTH(TRIM(UID)) = 60),
    FirstName VARCHAR(60) NOT NULL, 
    LastName VARCHAR(60) NOT NULL,
    Email VARCHAR(319) CHECK(TRIM(Email) LIKE '_%@_%._%')
 );
 
---enforce case-insensitive uniqueness of user e-mail addresses
+--Enforce case-insensitive uniqueness of user e-mail addresses
 CREATE UNIQUE INDEX idx_Unique_InstructorEmail
-ON User(LOWER(TRIM(Email)));
+ON ArcUser(LOWER(TRIM(Email)));
 
-
+--Define a table to store an Arc
+-- Primary Key: UID is the ID of the user that created the arc
 CREATE TABLE Arc
 (
-   UID VARCHAR(60) NOT NULL CHECK(LENGTH((TRIM(UID)) = 60)),
-   AID VARCHAR(60) NOT NULL CHECK(LENGTH((TRIM(AID)) = 60)),
+   UID VARCHAR(60) NOT NULL CHECK(CHAR_LENGTH(TRIM(UID)) = 60) REFERENCES ArcUser,
+   AID VARCHAR(60) NOT NULL CHECK(CHAR_LENGTH(TRIM(AID)) = 60),
    Title VARCHAR(60) NOT NULL,
    Description Text, 
    ParentArc VARCHAR(63) , 
-   FOREIGN KEY UID REFERENCES User.UID, 
-   PRIMARY KEY (UID AID)
+   PRIMARY KEY (UID, AID)
 );
 
-
+--Define a table to store a task
+-- Primary Key: AID and TID
+-- AID is the ID of the Arc the task belongs to
+-- TID is the ID of the task within this table 
 CREATE TABLE Task
 (
-   AID VARCHAR(60) NOT NULL CHECK(LENGTH((TRIM(AID)) = 60)),
-   TID VARCHAR(60) NOT NULL CHECK(LENGTH((TRIM(TID)) = 60)),
-   Title VARCHAR(60) NOT NULL CHECK(LENGTH((TRIM(Title)) > 0))
+   AID VARCHAR(60) NOT NULL CHECK(CHAR_LENGTH(TRIM(AID)) = 60),
+   UID VARCHAR(60) NOT NULL CHECK(CHAR_LENGTH(TRIM(UID)) = 60),
+   TID VARCHAR(60) NOT NULL CHECK(CHAR_LENGTH(TRIM(TID)) = 60),
+   Title VARCHAR(60) NOT NULL CHECK(CHAR_LENGTH(TRIM(Title)) > 0),
    Description TEXT, 
    DueDate TIMESTAMP,
    Location VARCHAR(256), 
-   FOREIGN KEY AID REFERENCES Arc.AID, 
+   FOREIGN KEY (AID, UID) REFERENCES Arc,
    PRIMARY Key (AID, TID)
 );
 
