b56adacb12e150d8c86e6c8557c265bdfe78256e
Justin Grabowski <justingrabowski@yahoo.com>
2019-04-14 16:59:53 -0400

    Add parentField
    Added the parentField to allow users to select a parent arc for the arc being created. Also fixed incorrect streams being set within widgets and added the navigator bar

--- client/src/arcplanner/lib/src/ui/add_arc_screen.dart
+++ client/src/arcplanner/lib/src/ui/add_arc_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import '../blocs/bloc.dart';
+import '../model/arc.dart';
+import 'drawer_menu.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 
 class AddArcScreen extends StatelessWidget {
+  
 
   Widget build(context) {
 
@@ -12,13 +16,32 @@ class AddArcScreen extends StatelessWidget {
         child: Column(
           children:[
             Container(margin: EdgeInsets.only(top: 20)),
-            locationField(),
             titleField(), //name
             //endDate(),
             descriptionField(),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              child: Row( 
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children:[ 
+                  parentField(),
+                  Flexible(
+                    child: Container(
+                      width:MediaQuery.of(context).size.width * 0.4 , 
+                      child: arcList(),
+                    ),
+                  ),   
+                ]
+              ),
+            ),
+            //parentField(),
+            //arcList(),
           ],
         ),
       ),
+
+      drawer: drawerMenu(context),
+
       bottomNavigationBar: BottomAppBar(
         color: Colors.blue,
         child: Row(
@@ -59,6 +82,7 @@ class AddArcScreen extends StatelessWidget {
     );
  }
 
+
  Widget titleField(){
   return StreamBuilder(
     stream: bloc.arcTitleFieldStream,
@@ -81,14 +105,14 @@ class AddArcScreen extends StatelessWidget {
 
 Widget endDate(){
   return StreamBuilder(
-    stream: bloc.arcTitleFieldStream,
+    stream: bloc.arcEndDateFieldStream,
     builder: (context, snapshot) {
       //Location
       //Name
       //End date
       //Description
       return TextField(
-        onChanged: bloc.changeTitle,
+        onChanged: bloc.changeEndDate,
         decoration: InputDecoration(
           hintText: 'EndDate'
           //errorText: snapshot.error,
@@ -100,7 +124,7 @@ Widget endDate(){
 
 Widget descriptionField(){
   return StreamBuilder(
-    stream: bloc.arcTitleFieldStream,
+    stream: bloc.arcDescriptionFieldStream,
     builder: (context, snapshot) {
       //Location
       //Name
@@ -116,6 +140,75 @@ Widget descriptionField(){
         ),
       );
     }
+  );
+}
+
+ Widget parentField(){
+  return StreamBuilder(
+      stream: bloc.arcParentFieldStream,
+      builder: (context, snapshot) {
+        return Text(snapshot.hasData? snapshot.data.title : "Parent");
+      }
+    );
+ }
+
+Widget arcList() {
+  
+String _arcTitle;
+
+  return StreamBuilder(
+    stream: bloc.arcViewStream,
+    builder: (context, snapshot) {
+      return new FutureBuilder(
+        future: snapshot.data,
+        builder: (context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.hasData) {
+          dynamic snapshotData = snapshot.data;
+          return DropdownButton<dynamic>(
+            hint: Text("Select Parent Arc"),
+            value: _arcTitle,
+            onChanged: (value) {
+              bloc.changeParent(value);
+              print("$value");
+            },
+            isExpanded: true,
+            items: List<DropdownMenuItem<dynamic>>.generate(
+            snapshotData.length,
+              (int index) => DropdownMenuItem<dynamic>(
+                value: snapshotData[index],
+                child: Text(snapshotData[index].title),
+              ),
+            ),
+          );
+           } else {
+            return Text('There are no Arcs/Tasks');
+          }   
+        },
+      );
+    },
+  );
+}
+
+Widget arcTile(Arc arc, BuildContext context) {
+  var description = arc.description;
+  if (description == null) {
+    description = '';
+  }
+
+  return Container(
+    decoration: BoxDecoration(
+      border: Border(
+        bottom: BorderSide(
+          color: Colors.grey[350],
+        ),
+        top: BorderSide(
+          color: Colors.grey[350],
+        ),
+      ),
+    ),
+    child: ListTile(
+      title: Text(arc.title),
+    ),
   );
 }
 
