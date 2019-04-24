6bd52b56b4a9e53f14f8066a791a30013f7dfda2
Matthew Chastain <matthiuschastain@gmail.com>
2019-04-24 09:28:45 -0400

    Update databaseHelper.dart

--- client/src/arcplanner/lib/src/util/databaseHelper.dart
+++ client/src/arcplanner/lib/src/util/databaseHelper.dart
@@ -188,8 +188,10 @@ class DatabaseHelper {
   // Pulls all Arcs and Tasks that are due in the next 7 days from DB into app
   Future<List<Map>> getUpcomingItems() async {
     var dbClient = await db;
-    List<Map> arcList = await dbClient.rawQuery('SELECT * FROM Arc WHERE DueDate < (date(''now'') + 7)');
-    List<Map> taskList = await dbClient.rawQuery('SELECT * FROM Task WHERE DueDate < (date(''now'') + 7)');
+    List<Map> arcList = await dbClient.rawQuery('SELECT * FROM Arc WHERE DueDate <= (date(\'now\') + 7)');
+    print(arcList);
+    List<Map> taskList = await dbClient.rawQuery('SELECT * FROM Task WHERE DueDate <= (date(\'now\') + 7)');
+    print(taskList);
     return new List.from(arcList)..addAll(taskList);
   }
 
