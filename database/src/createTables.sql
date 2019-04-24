4fafac65760d829b3e7c695fdb97349384cc0f95
Kevin Kelly <34460255+KevinKelly25@users.noreply.github.com>
2019-04-24 13:07:50 -0400

    Update createTables.sql
    Removed unneeded checks, added fields that were missing, and updated header comment

--- database/src/createTables.sql
+++ database/src/createTables.sql
@@ -1,18 +1,16 @@
---createTables.sql - ArcPlanner
-
---Matthew Chastain, Justin Grabowski, Kevin Kelly, Jonathan Middleton
--- CS298 Spring 2019 Team CGKM
-
---Version 1
-
---*Add licence*
-
---PROVIDED AS IS. NO WARRANTIES EXPRESSED OR IMPLIED. USE AT YOUR OWN RISK.
-
---This script creates schema, tables, and indexes for the arcPlanner application
-
---E-mail address management is based on the discussion presented at:
--- https://gist.github.com/smurthys/feba310d8cc89c4e05bdb797ca0c6cac
+/** 
+ *  Team CGKM - Matthew Chastain, Justin Grabowski, Kevin Kelly, Jonathan Middleton
+ *  CS298 Spring 2019 
+ *
+ *  Authors: 
+ *    Primary: Justin Grabowski, Kevin Kelly
+ *    Contributors: 
+ * 
+ *  Provided as is. No warranties expressed or implied. Use at your own risk.
+ *
+ *  This script creates schema, tables, and indexes for the arcPlanner server
+ *   application
+ */
 
 
 --The results of running this script will be spooled to 
@@ -38,7 +36,7 @@ START TRANSACTION;
 -- UID is the ID of the user within the table
 CREATE TABLE ArcUser
 (
-   UID VARCHAR(60) PRIMARY KEY CHECK(CHAR_LENGTH(TRIM(UID)) = 60),
+   UID VARCHAR(60) PRIMARY KEY,
    FirstName VARCHAR(60) NOT NULL, 
    LastName VARCHAR(60) NOT NULL,
    Email VARCHAR(319) CHECK(TRIM(Email) LIKE '_%@_%._%')
@@ -53,11 +51,13 @@ ON ArcUser(LOWER(TRIM(Email)));
 -- AID is the ID of the Arc within the table
 CREATE TABLE Arc
 (
-   AID VARCHAR(60) NOT NULL PRIMARY KEY CHECK(CHAR_LENGTH(TRIM(AID)) = 60),
+   AID VARCHAR(60) NOT NULL PRIMARY KEY,
    UID VARCHAR(60) NOT NULL REFERENCES ArcUser,
    Title VARCHAR(60) NOT NULL,
-   Description Text, 
-   ParentArc VARCHAR(63)
+   Description Text,
+   DueDate DATE,
+   ParentArc VARCHAR(63),
+   Completed BOOLEAN
 );
 
 --Define a table to store a task
@@ -65,12 +65,13 @@ CREATE TABLE Arc
 -- TID is the ID of the Task within this table 
 CREATE TABLE Task
 (
-   TID VARCHAR(60) NOT NULL PRIMARY KEY CHECK(CHAR_LENGTH(TRIM(TID)) = 60),
+   TID VARCHAR(60) NOT NULL PRIMARY KEY,
    AID VARCHAR(60) NOT NULL REFERENCES Arc,
    Title VARCHAR(60) NOT NULL CHECK(CHAR_LENGTH(TRIM(Title)) > 0),
    Description TEXT, 
    DueDate TIMESTAMP,
-   Location VARCHAR(256)
+   Location VARCHAR(256),
+   Completed BOOLEAN
 );
 
 
